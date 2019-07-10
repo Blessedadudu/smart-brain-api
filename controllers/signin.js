daf45fbdbfc7aa9cf07af7e7055a972432eb1949
Blessedadudu <47356974+Blessedadudu@users.noreply.github.com>
2019-07-10 15:01:33 +0100

    signin.js update

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,7 +1,8 @@
 const handleSignin = (req, res, db, bcrypt)=> {
+	// const { email, name, password} = req.body;
 	if (!email || !password ) {
 		return res.status(400).json('incorrect form submission')
-	}
+	} 
 	db.select('email', 'hash').from('login')
 	.where('email', '=', req.body.email)
 	.then(data =>{
