8ebdd6402de72473bf5e99ee1d8cb7eff02df772
Blessedadudu <47356974+Blessedadudu@users.noreply.github.com>
2019-07-10 10:05:37 +0100

    new file

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai')
+
+/* ADD YOUR OWN API KEY */
+const app = new Clarifai.App({
+ apiKey: '20aa94b1e29d4b37ab39507005a7553a'
+});
+
+
+const handleApiCall = (req,res) => {
+	app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+    	res.json(data);
+    })
+    .catch(err => res.status(400).json('UNABLE TO WORK WITH API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+    	res.json(entries);
+  })
+    .catch(err => res.status(404).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage, 
+	handleApiCall /* Es6 feature - You don't need to pass in the value */
+}
