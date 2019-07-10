586157c0117574efff4ba3403f1870656a6d469c
Blessedadudu <47356974+Blessedadudu@users.noreply.github.com>
2019-07-10 12:57:14 +0100

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',  
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-animate-17681',
     user : 'postgres',
     password : '123490',
     database : 'imagerecognition'
